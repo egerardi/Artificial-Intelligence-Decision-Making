42bb99592049090c375c029a87591e86651356ff
Eric <egerardi24@gmail.com>
2017-02-14 20:37:51 -0500

    converted to regular Java class, reference MasterGameProcess.frame

--- Framework.java
+++ Framework.java
@@ -2,14 +2,20 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-@SuppressWarnings("serial")
-public class Framework extends JFrame {
+
+public class Framework {
 	
-	private JLabel [][] grid;
+	private static JLabel [][] grid;
 	private int gridPieceDimension = 25;
 	
 	public Framework (int numRows, int numCols) {
 		
+		initalizeGrid(numRows, numCols);
+		
+		setPortal(numRows - 2, numCols - 1);
+	}
+	
+	private void initalizeGrid (int numRows, int numCols) {
 		grid = new JLabel[numRows][numCols];
 		
 		for (int row = 0; row < numRows; row++) 
@@ -21,23 +27,32 @@ public class Framework extends JFrame {
 				
 				if (row == numRows - 1 || col == numCols - 1 || row == 0 || col == 0 ) //If row or col is the border, set to wall
 				{
-					grid[row][col].setIcon(new ImageIcon(GameboardPiece.getWall()));
+					grid[row][col].setIcon(new ImageIcon( GameboardPiece.getWall() ));
 				}
 				else
 				{
-					grid[row][col].setIcon(new ImageIcon(GameboardPiece.getEmpty()));
+					grid[row][col].setIcon(new ImageIcon( GameboardPiece.getEmpty() ));
 				}
 				
 				grid[row][col].setName( "r" + Integer.toString(row) + "c" + Integer.toString(col) );
-				this.add(grid[row][col]);
+				MasterGameProcess.frame().add(grid[row][col]);
 			}
 		}
 		
-		setSize((numCols + 1) * gridPieceDimension - 8, (numRows + 7) * gridPieceDimension);
-		setLayout(null);
-		setVisible(true);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		MasterGameProcess.frame().setSize((numCols + 1) * gridPieceDimension - 8, (numRows + 7) * gridPieceDimension);
+		MasterGameProcess.frame().setLayout(null);
+		MasterGameProcess.frame().setVisible(true);
+		MasterGameProcess.frame().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
+	
+	private void setPortal (int row, int col) { //Set the portal (with class instantiation)
+        grid[row][col].setIcon(new ImageIcon( GameboardPiece.getPortal() ));
+    }
+	
+	public void setPlayerPosition (int rowOld, int colOld, int row, int col) {
+		grid[rowOld][colOld].setIcon(new ImageIcon( GameboardPiece.getEmpty() )); //Remove player old position
+		grid[row][col].setIcon(new ImageIcon( GameboardPiece.getPlayer() ));  //Set player new position
+    }
 	
 	
 }
