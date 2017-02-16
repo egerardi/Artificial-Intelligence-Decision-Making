8cc0eac2033a36f6ef2d52c14355d75020a1daa6
Eric <egerardi24@gmail.com>
2017-02-15 23:39:11 -0500

    made directionCounter:Int into class variable

--- DecisionFactory.scala
+++ DecisionFactory.scala
@@ -23,27 +23,12 @@ object DecisionFactory {
   var didFirstDown = false;
   var hitFirstDown = false;
   var alongWall = 0; //Same integers as lastMove, but 0 == Not along wall
+  var directionCounter : Int = 0;
   var upDownLeftRight = ofDim[Int](4,5);
-      upDownLeftRight(0)(0) = 0;
-      upDownLeftRight(0)(1) = 1;
-      upDownLeftRight(0)(2) = 2;
-      upDownLeftRight(0)(3) = 3;
-      upDownLeftRight(0)(4) = 4;
-      upDownLeftRight(1)(0) = 0;
-      upDownLeftRight(1)(1) = 1;
-      upDownLeftRight(1)(2) = 2;
-      upDownLeftRight(1)(3) = 4;
-      upDownLeftRight(1)(4) = 3;
-      upDownLeftRight(2)(0) = 0;
-      upDownLeftRight(2)(1) = 4;
-      upDownLeftRight(2)(2) = 3;
-      upDownLeftRight(2)(3) = 2;
-      upDownLeftRight(2)(4) = 1;
-      upDownLeftRight(3)(0) = 0;
-      upDownLeftRight(3)(1) = 4;
-      upDownLeftRight(3)(2) = 3;
-      upDownLeftRight(3)(3) = 1;
-      upDownLeftRight(3)(4) = 2;
+      upDownLeftRight(0)(0) = 0; upDownLeftRight(0)(1) = 1; upDownLeftRight(0)(2) = 2; upDownLeftRight(0)(3) = 3; upDownLeftRight(0)(4) = 4; //Stay Up Down Left Right
+      upDownLeftRight(1)(0) = 0; upDownLeftRight(1)(1) = 1; upDownLeftRight(1)(2) = 2; upDownLeftRight(1)(3) = 4; upDownLeftRight(1)(4) = 3; //Stay Up Down Right Left
+      upDownLeftRight(2)(0) = 0; upDownLeftRight(2)(1) = 4; upDownLeftRight(2)(2) = 3; upDownLeftRight(2)(3) = 2; upDownLeftRight(2)(4) = 1; //Stay Right Left Down Up
+      upDownLeftRight(3)(0) = 0; upDownLeftRight(3)(1) = 4; upDownLeftRight(3)(2) = 3; upDownLeftRight(3)(3) = 1; upDownLeftRight(3)(4) = 2; //Stay Right Left Up Down
   
   // Log level (to console)
   var logLevel = 1
@@ -56,8 +41,8 @@ object DecisionFactory {
   //   3 - move left
   //   4 - move right
   def Decision() : Int = {
-    noBackToBackRepeat();
-//    zigzagWalk();
+//    noBackToBackRepeat();
+    zigzagWalk();
     //_sweepRoomModelDecision()
   }
 
@@ -98,85 +83,88 @@ object DecisionFactory {
   }
   
   // Internal decision function, spiral walk
-//  private def zigzagWalk() : Int = {
-//
-//    var temp = lastMove;
-//    var directionCounter = 0;
-//    
-//    if (lastSignal == -1 && lasLastSignal == -1)
-//    {
-//        directionCounter = directionCounter + 1;
-//        
-//        println("Hi" + directionCounter);
-//        if (lastMove == 1)
-//        {
-//            lastMove = 2;
-//        }
-//        else if (lastMove == 2)
-//        {
-//            lastMove = 1;
-//        }
-//        else if (lastMove == 3)
-//        {
-//            lastMove = 4;
-//        }
-//        else if (lastMove == 4)
-//        {
-//            lastMove = 3;
-//        }
-//        else
-//        {
-//          
-//        }   
-//    }
-//    else
-//    {
-//        if (lastSignal == -999) //Beginning move
-//        {
-//            lastMove = upDownLeftRight(directionCounter)(3); //Left
-//        }
-//        else
-//        {
-//            if (lastSignal == -1) //If last movement decision failed
-//            {
-//                if (didFirstDown == false)
-//                {
-//                    lastMove = upDownLeftRight(directionCounter)(2); //Down
-//                    didFirstDown = true;
-//                }
-//                else if (didFirstDown == true && hitFirstDown == false)
-//                {
-//                    lastMove = upDownLeftRight(directionCounter)(1);  //Up
-//                    hitFirstDown = true;
-//                }
-//                else if (lastMove == upDownLeftRight(directionCounter)(1) || lastMove == upDownLeftRight(directionCounter)(2)) //If lastMove was Up or Down
-//                {
-//                    lastMove = upDownLeftRight(directionCounter)(4); //Right
-//                }
-//                else 
-//                {
-//                  
-//                }           
-//            }
-//            else 
-//            {
-//                if (lastMove == upDownLeftRight(directionCounter)(4))
-//                {
-//                    if (lastLastMove == upDownLeftRight(directionCounter)(1)) //If lastMove right && lastLastMove up
-//                    {
-//                        lastMove = upDownLeftRight(directionCounter)(2); //Down
-//                    }
-//                    else //(lastLastMove == 2) //If lastMove right && lastLastMove down
-//                    {
-//                        lastMove = upDownLeftRight(directionCounter)(1);  //Up
-//                    }
-//                }  
-//            }
-//        }
-//    }
-//    lastLastMove = temp;
-//
-//    // Returns newly created move:
-//    lastMove
-//  }  
+  private def zigzagWalk() : Int = {
+
+    var temp = lastMove;
+    
+    if (lastSignal == -1 && lasLastSignal == -1)
+    {
+        directionCounter = directionCounter + 1;
+        
+        if (lastLastMove == 1)
+        {
+            lastMove = 2;
+        }
+        else if (lastLastMove == 2)
+        {
+            lastMove = 1;
+        }
+        else if (lastLastMove == 3)
+        {
+            lastMove = 4;
+        }
+        else if (lastLastMove == 4)
+        {
+            lastMove = 3;
+        }
+        else
+        {
+          
+        }   
+    }
+    else
+    {
+        if (lastSignal == -999) //Beginning move
+        {
+            lastMove = upDownLeftRight(directionCounter)(3); //Left
+        }
+        else
+        {
+            if (lastSignal == -1) //If last movement decision failed
+            {
+                if (didFirstDown == false)
+                {
+                    lastMove = upDownLeftRight(directionCounter)(2); //Down
+                    didFirstDown = true;
+                }
+                else if (didFirstDown == true && hitFirstDown == false)
+                {
+                    lastMove = upDownLeftRight(directionCounter)(1);  //Up
+                    hitFirstDown = true;
+                }
+                else if (lastMove == upDownLeftRight(directionCounter)(1) || lastMove == upDownLeftRight(directionCounter)(2)) //If lastMove was Up or Down
+                {
+                    lastMove = upDownLeftRight(directionCounter)(4); //Right
+                }
+                else 
+                {
+                  
+                }           
+            }
+            else 
+            {
+                if (lastMove == upDownLeftRight(directionCounter)(4))
+                {
+                    if (lastLastMove == upDownLeftRight(directionCounter)(1)) //If lastMove right && lastLastMove up
+                    {
+                        lastMove = upDownLeftRight(directionCounter)(2); //Down
+                    }
+                    else //(lastLastMove == 2) //If lastMove right && lastLastMove down
+                    {
+                        lastMove = upDownLeftRight(directionCounter)(1);  //Up
+                    }
+                }  
+            }
+        }
+    }
+    
+    println("directionCounter " + directionCounter);
+    println("lastMove " + lastMove);
+    println("lastLastMove " + lastLastMove);
+    
+    lastLastMove = temp;
+
+    // Returns newly created move:
+    lastMove
+  }  
 }
