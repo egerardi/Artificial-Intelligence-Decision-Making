1c5c7a2f2f1ad7a25fe03a28ca8b8b50bd36fc2a
Eric <egerardi24@gmail.com>
2017-02-18 13:56:17 -0500

    create Vertex.scala

---
+++ Vertex.scala
@@ -0,0 +1,21 @@
+
+
+object Vertex {
+    private var x : Int = _;
+    private var y : Int = _;
+    
+    def getX () : Int = {
+      return x;
+    }
+    def setX (newX : Int) {
+      x = newX;
+    }
+    
+    def getY () : Int = {
+      return y;
+    }
+    def setY (newY : Int) {
+      y = newY;
+    }
+    
+}
