af2cedf8b0719e70e5a4715317b5130326fa6143
Eric <egerardi24@gmail.com>
2017-02-18 20:01:31 -0500

    add beginning part of WallRun() algorithm

--- DecisionFactory.scala
+++ DecisionFactory.scala
@@ -1,7 +1,9 @@
 //package mazeRunner
 import Array._
+import java.util.Stack;
 
 import scala.util.Random
+//import scala.scalajs.js
 import scala.collection.mutable.ArrayBuffer  // For mutable, scalable arrays
 
 /** Author: Eric Gerardi
@@ -15,139 +17,123 @@ import scala.collection.mutable.ArrayBuffer  // For mutable, scalable arrays
 // Main decision factory object:
 object DecisionFactory {
 
-  // Mutable object attributes:
-  var lastSignal = -999 // note: last signal of -999 means the event has just started
-  var lasLastSignal = -999;
-  var lastMove = 0      // of possibility utility
-  var lastLastMove = 0;
-  var didFirstDown = false;
-  var hitFirstDown = false;
-  var alongWall = 0; //Same integers as lastMove, but 0 == Not along wall
-  var directionCounter : Int = 0;
-  var upDownLeftRight = ofDim[Int](4,5);
-      upDownLeftRight(0)(0) = 0; upDownLeftRight(0)(1) = 1; upDownLeftRight(0)(2) = 2; upDownLeftRight(0)(3) = 3; upDownLeftRight(0)(4) = 4; //Stay Up Down Left Right
-      upDownLeftRight(1)(0) = 0; upDownLeftRight(1)(1) = 1; upDownLeftRight(1)(2) = 2; upDownLeftRight(1)(3) = 4; upDownLeftRight(1)(4) = 3; //Stay Up Down Right Left
-      upDownLeftRight(2)(0) = 0; upDownLeftRight(2)(1) = 4; upDownLeftRight(2)(2) = 3; upDownLeftRight(2)(3) = 2; upDownLeftRight(2)(4) = 1; //Stay Right Left Down Up
-      upDownLeftRight(3)(0) = 0; upDownLeftRight(3)(1) = 4; upDownLeftRight(3)(2) = 3; upDownLeftRight(3)(3) = 1; upDownLeftRight(3)(4) = 2; //Stay Right Left Up Down
+    // Mutable object attributes:
+    var lastSignal = -999 // note: last signal of -999 means the event has just started
+    var lasLastSignal = -999;
+    var lastMove = 0      // of possibility utility
+    var lastLastMove = 0;
+    var alongWall = 0; //Same integers as lastMove, but 0 == Not along wall
+    var directionCounter : Int = 0;
+    var upDownLeftRight = ofDim[Int](4,5);
+    
+    
+    //Array Buffer for queue of Vertices
+    var queue = ArrayBuffer[Vertex]();
+    //Add first vertex (Player start is considered 0,0)
+    var v : Vertex = new Vertex(0,0);
+    queue.append(v);
+    
+    // Log level (to console)
+    var logLevel = 1
   
-  // Log level (to console)
-  var logLevel = 1
-
-  // Main Decision engine:
-  //   Direction choices to return:
-  //   0 - stay (don't move)
-  //   1 - move up
-  //   2 - move down
-  //   3 - move left
-  //   4 - move right
-  def Decision() : Int = {
-
-    zigzagWalk();
-  }
-
-  // Signal channel for receiving results about decisions and the environment:
-  //   Signals are:
-  //   -999 - starting round
-  //   -1   - last movement decision failed
-  //   1    - last movement decision worked
-  def receiveSignal( sig: Int ) = {
-//    sig match {
-//      case -1 => { if (logLevel == 1) println("SIG REC: wall collision") }
-//      case 1  => { if (logLevel == 1) println("SIG REC: successful move"); }
-//      case 2  => { if (logLevel == 1) println("SIG REC: PORTALATED!!"); }
-//      case _  => { ;; }
-//    }
-
-    // Record latest signal in attribute:
-    lasLastSignal = lastSignal;
-    lastSignal = sig 
-  }  
-
+    // Main Decision engine:
+    //   Direction choices to return:
+    //   0 - stay (don't move)
+    //   1 - move up
+    //   2 - move down
+    //   3 - move left
+    //   4 - move right
+    def Decision() : Int = {
+      WallRun();
+    }
   
-  // Internal decision function, spiral walk
-  private def zigzagWalk() : Int = {
-
-    var temp = lastMove;
+    // Signal channel for receiving results about decisions and the environment:
+    //   Signals are:
+    //   -999 - starting round
+    //   -1   - last movement decision failed
+    //   1    - last movement decision worked
+    def receiveSignal( sig: Int ) = {
+  //    sig match {
+  //      case -1 => { if (logLevel == 1) println("SIG REC: wall collision") }
+  //      case 1  => { if (logLevel == 1) println("SIG REC: successful move"); }
+  //      case 2  => { if (logLevel == 1) println("SIG REC: PORTALATED!!"); }
+  //      case _  => { ;; }
+  //    }
+  
+      // Record latest signal in attribute:
+      lasLastSignal = lastSignal;
+      lastSignal = sig 
+    }  
+  
     
-    if (lastSignal == -1 && lasLastSignal == -1)
-    {
-        directionCounter = directionCounter + 1;
+    // Internal decision function, Wall Run
+    private def WallRun() : Int = {
+//        println(queue.last.getX() + " " + queue.last.getY());
         
-        if (lastLastMove == 1)
+        println(lastSignal);
+        if (lastSignal == -1)
         {
-            lastMove = 2;
+            queue.remove(queue.length - 1);
         }
-        else if (lastLastMove == 2)
+      
+        lastLastMove = lastMove;
+      
+        //Get Character Current X, Y
+        var currentX : Int = queue.last.getX();
+        var currentY : Int = queue.last.getY();
+        
+        //Add all surrounding Vertices (of the current one)
+        if ( !( queue.exists { x => x.getX() == currentX } && queue.exists { y => y.getY() == currentY + 1 } ) ) //If grid Vertex 1 Up does not exist
         {
-            lastMove = 1;
+            var v : Vertex = new Vertex(currentX, currentY + 1); //Create Vertex
+            queue.append(v); //Append Vertex
         }
-        else if (lastLastMove == 3)
+        if ( !( queue.exists { x => x.getX() == currentX + 1 } && queue.exists { y => y.getY() == currentY } ) ) //If grid Vertex 1 Right does not exist
         {
-            lastMove = 4;
+            var v : Vertex = new Vertex(currentX + 1, currentY); //Create Vertex
+            queue.append(v); //Append Vertex
         }
-        else if (lastLastMove == 4)
+        if ( !( queue.exists { x => x.getX() == currentX } && queue.exists { y => y.getY() == currentY - 1 } ) ) //If grid Vertex 1 Down does not exist
         {
-            lastMove = 3;
+            var v : Vertex = new Vertex(currentX, currentY - 1); //Create Vertex
+            queue.append(v); //Append Vertex
         }
-        else
+        if ( !( queue.exists { x => x.getX() == currentX - 1} && queue.exists { y => y.getY() == currentY } ) ) //If grid Vertex 1 Left does not exist
         {
-          
+            var v : Vertex = new Vertex(currentX - 1, currentY); //Create Vertex
+            queue.append(v); //Append Vertex
         }
         
-        if (directionCounter == 2)
+        //Move Character according to last Vertex
+        if (queue.last.getX() == currentX && queue.last.getY() == currentY + 1)
         {
-            lastMove = 4;
+            lastMove = 1; //Move Up
         }
-    }
-    else
-    {
-        if (lastSignal == -999) //Beginning move
+        else if (queue.last.getX() == currentX && queue.last.getY() == currentY - 1)
         {
-            lastMove = upDownLeftRight(directionCounter)(3); //Left
+            lastMove = 2; //Move Down
         }
-        else
+        else if (queue.last.getX() == currentX - 1 && queue.last.getY() == currentY)
         {
-            if (lastSignal == -1) //If last movement decision failed
-            {
-                if (didFirstDown == false)
-                {
-                    lastMove = upDownLeftRight(directionCounter)(2); //Down
-                    didFirstDown = true;
-                }
-                else if (didFirstDown == true && hitFirstDown == false)
-                {
-                    lastMove = upDownLeftRight(directionCounter)(1);  //Up
-                    hitFirstDown = true;
-                }
-                else if (lastMove == upDownLeftRight(directionCounter)(1) || lastMove == upDownLeftRight(directionCounter)(2)) //If lastMove was Up or Down
-                {
-                    lastMove = upDownLeftRight(directionCounter)(4); //Right
-                }
-                else 
-                {
-                  
-                }           
-            }
-            else 
-            {
-                if (lastMove == upDownLeftRight(directionCounter)(4))
-                {
-                    if (lastLastMove == upDownLeftRight(directionCounter)(1)) //If lastMove right && lastLastMove up
-                    {
-                        lastMove = upDownLeftRight(directionCounter)(2); //Down
-                    }
-                    else //(lastLastMove == 2) //If lastMove right && lastLastMove down
-                    {
-                        lastMove = upDownLeftRight(directionCounter)(1);  //Up
-                    }
-                }  
-            }
+            lastMove = 3; //Move Left
         }
-    }
-    
-    lastLastMove = temp;
-    
-    // Returns newly created move:
-    lastMove
-  }  
+        else //if (queue.last.getX() == currentX + 1 && queue.last.getY() == currentY)
+        {
+            lastMove = 4; //Move Right
+        }
+        
+        for ( i <- queue)
+        {
+            println(i.getX() + " " + i.getY());
+        }
+        println();
+//        println(queue.last.getX() + " " + queue.last.getY());
+        
+      
+      
+        // Returns newly created move:
+        lastMove
+    }  
 }
+
+
